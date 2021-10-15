70b31860e566a462762dd5bdd6c30fdd7d39b55c
nganzalo <nganzalo@buffalo.edu>
2021-10-15 16:35:17 -0400

    plugin complte

--- src/main/java/com/HuePlugin/Fireworks.java
+++ src/main/java/com/HuePlugin/Fireworks.java
@@ -25,7 +25,7 @@ public class Fireworks extends Thread {
                 java.awt.Color.RED,java.awt.Color.PINK, java.awt.Color.YELLOW
         };
 
-        for (int i = 0; i<10; i++){
+        for (int i = 0; i<7; i++){
             for (int j = 0; j<colArray.length; j++) {
 
 
